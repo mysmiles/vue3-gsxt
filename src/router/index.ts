b04c02cd988f77b8fcdd1f13c691463e956696a5
mysmiles <1490767581@qq.com>
2021-12-07 09:03:13 +0800

    router and store

---
+++ src/router/index.ts
@@ -0,0 +1,13 @@
+import {createRouter, createWebHashHistory} from 'vue-router'
+
+const routes = []
+
+const router = createRouter({
+  // 指定路由的模式,此处使用的是hash模式
+  history: createWebHashHistory(),
+  // 路由地址
+  routes: []
+})
+
+// 在 Vue-router新版本中，需要使用createRouter来创建路由
+export default router
